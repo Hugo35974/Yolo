426a397b0781fc50f3dbe845dc45ce2923fa590b
Hugo Bénard <>
2024-02-09 18:54:56 +0100

    no message

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
 import cv2
 import torch
 
-model = torch.hub.load('ultralytics/yolov5:master', 'yolov5s')
+# Charger YOLOv5 depuis torch.hub
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
-cap = cv2.VideoCapture(0)
+# Ouvrir la vidéo
+video_path = 'sample-5s.mp4'  # Remplacez par le chemin de votre vidéo
+cap = cv2.VideoCapture(video_path)
 
 while True:
+    # Lire l'image de la vidéo
     ret, frame = cap.read()
 
+    # Vérifier si la lecture de l'image a réussi
     if not ret:
-        print("Erreur lors de la lecture de l'image de la caméra.")
+        print("Fin de la vidéo.")
         break
+
+    # Convertir l'image OpenCV (BGR) en format RGB
     im = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+    # Effectuer l'inférence avec YOLOv5
     results = model(im)
+
+    # Récupérer les prédictions
     predictions = results.xyxy[0].numpy()
+
+    # Dessiner les bounding boxes sur l'image
     for box in predictions:
         x_min, y_min, x_max, y_max = box[0:4].astype(int)
         cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
 
-    cv2.imshow('Camera', frame)
-
+    # Afficher l'image
+    cv2.imshow('Video', frame)
+    results.print()
+    results.save()
+    # Quitter la boucle si la touche 'q' est pressée
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# Libérer la vidéo et fermer les fenêtres
 cap.release()
 cv2.destroyAllWindows()
